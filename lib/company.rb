097183db98b84fe4f6370f4f65b0d4dd44bf8702
Manoj Panta <manojpanta95@gmail.com>
2018-04-17 10:29:54 -0600

    working how to raise an error

--- lib/company.rb
+++ lib/company.rb
@@ -1,3 +1,9 @@
+require_relative 'project'
+require_relative 'employee'
+require_relative 'timesheet'
+require 'csv'
+
+
 class Company
 
   attr_reader :employees,
@@ -10,11 +16,27 @@ class Company
     @employees = []
     @projects = []
     @timesheets = []
+    load_employees(path[:employees])
+    load_projects(path[:projects])
+    load_timesheets(path[:timesheets])
   end
 
-  def self.load_employees(path)
-    CSV.foreach(path, headers: true, header_converters: :symbol) do |data|
-      merchant = Merchant.new(data, self)
+  def load_employees(path = path[:employees])
+    CSV.foreach(path, headers: false, header_converters: :symbol) do |data|
+      @employees <<  Employee.new(data[0], data[1], data[2], data[3], data[4])
+    end
+  end
+
+  def load_projects(path = path[:projects])
+    CSV.foreach(path, headers: false, header_converters: :symbol) do |data|
+      @projects << Project.new(data[0], data[1], data[2], data[3])
+    end
+  end
+
+  def load_timesheets(path = path[:timesheets])
+    CSV.foreach(path, headers: false, header_converters: :symbol) do |data|
+      @timesheets << TimeSheet.new(data[0], data[1], data[2], data[3])
+    end
   end
 
 
--- test/company_test.rb
+++ test/company_test.rb
@@ -4,13 +4,18 @@ require './lib/company'
 class CompanyTest < Minitest::Test
 
   def test_it_exists
-    company = Company.new({:employees => './data/employees.csv'
-                            :projects => './data/projects.csv'
-                            :timesheets => './data/timesheets.csv' })
+    company = Company.new({:employees => './data/employees.csv',
+                            :projects => './data/projects.csv',
+                            :timesheets => './data/timesheets.csv'})
 
     assert_instance_of Company, company
+    assert_equal 3, company.projects.length
+    assert_equal 2, company.employees.length
+    assert_equal 25, company.timesheets.length
 
-
+    assert_instance_of Employee, company.employees.first
+    assert_instance_of Project, company.projects.first
+    assert_instance_of TimeSheet, company.timesheets.first
   end
 
 
