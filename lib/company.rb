137e774239d8d369f159d74267b7c92c7251a684
Manoj Panta <manojpanta95@gmail.com>
2018-04-17 10:40:24 -0600

    added error hash

--- lib/company.rb
+++ lib/company.rb
@@ -23,19 +23,32 @@ class Company
 
   def load_employees(path = path[:employees])
     CSV.foreach(path, headers: false, header_converters: :symbol) do |data|
-      @employees <<  Employee.new(data[0], data[1], data[2], data[3], data[4])
+      if data.length == 5
+        @employees <<  Employee.new(data[0], data[1], data[2], data[3], data[4])
+      else
+        return {success: false, error: 'bad_data'}
+      end
+
     end
   end
 
   def load_projects(path = path[:projects])
     CSV.foreach(path, headers: false, header_converters: :symbol) do |data|
-      @projects << Project.new(data[0], data[1], data[2], data[3])
+      if data.length == 4
+        @projects << Project.new(data[0], data[1], data[2], data[3])
+      else
+        return {success: false, error: 'bad_data'}
+      end
     end
   end
 
   def load_timesheets(path = path[:timesheets])
     CSV.foreach(path, headers: false, header_converters: :symbol) do |data|
-      @timesheets << TimeSheet.new(data[0], data[1], data[2], data[3])
+      if data.length == 4
+        @timesheets << TimeSheet.new(data[0], data[1], data[2], data[3])
+      else
+        return {success: false, error: 'bad_data'}
+      end
     end
   end
 
